e040a2d80460f4b0b28a614e78d939c2ceda3794
DhruvvGitHub <dhruvtanwani967@gmail.com>
2025-01-10 17:00:27 +0530

    animation completed

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var tl = gsap.timeline({
-    repeat: -1,  
+    repeat: -1,
     onStart: function() {
-        gsap.set('.image-1', { left: '0%' });
+        gsap.set('.image-1', { left: '0%', scale: 0.5 });
         gsap.set('.image-2', { left: '30%' });
-        gsap.set('.image-3', { left: '60%' });
+        gsap.set('.image-3', { left: '60%', scale: 0.5 });
     }
 });
 
@@ -12,15 +12,24 @@ tl.to(".image", {
     ease: "power1.inOut",
     onStart: function() {
         document.querySelectorAll('.image').forEach(image => {
-            let left = window.getComputedStyle(image).getPropertyValue('left');
-            console.log(left);
+            // Get the left position in pixels
+            let leftPx = parseFloat(window.getComputedStyle(image).getPropertyValue('left'));
             
-            if (left === '0px') {
-                gsap.to(image, { left: '60%', duration: 1 });
-            } else if (left === '276.475px') {
-                gsap.to(image, { left: '0%', duration: 1 });
-            } else if (left === '552.95px') {
-                gsap.to(image, { left: '30%',zIndex: 2, duration: 1 });
+            // Get the parent container's width
+            let parentWidth = image.parentElement.offsetWidth;
+            console.log(parentWidth);
+            
+            // Calculate the left value in percentage
+            let leftPercentage = (leftPx / parentWidth) * 100;
+            // console.log(leftPercentage);
+            
+            if (leftPercentage === 0) {
+                gsap.to(image, { left: '60%', duration: 1, scale: 0.5, zIndex: 1 });
+            } else if (leftPercentage > 29 & leftPercentage <= 31) {
+                gsap.to(image, { left: '0%', duration: 1, scale: 0.5, zIndex: 1 });
+            } 
+            else if (leftPercentage > 59 & leftPercentage <= 61) {
+                gsap.to(image, { left: '30%', zIndex: 2, duration: 1, scale: 1 });
             }
         });
     },
