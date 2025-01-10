44a7369a0689c9fe60113fe72bd61667c0e10397
DhruvvGitHub <dhruvtanwani967@gmail.com>
2025-01-10 22:53:21 +0530

    reduced space b/w elements

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var tl = gsap.timeline({
     repeat: -1,
     onStart: function() {
-        gsap.set('.image-1', { left: '0%', scale: 0.5 });
-        gsap.set('.image-2', { left: '30%' });
-        gsap.set('.image-3', { left: '60%', scale: 0.5 });
+        gsap.set('.image-1', { left: '0%', scale: 0.6 });
+        gsap.set('.image-2', { left: '30%', scale: 1.2 });
+        gsap.set('.image-3', { left: '60%', scale: 0.6 });
     }
 });
 
@@ -23,12 +23,12 @@ tl.to(".image", {
             // console.log(leftPercentage);
             
             if (leftPercentage === 0) {
-                gsap.to(image, { left: '60%', duration: 1.5, scale: 0.5, zIndex: 0 });
+                gsap.to(image, { left: '60%', duration: 1.5, scale: 0.6, zIndex: 0 });
             } else if (leftPercentage > 29 & leftPercentage <= 31) {
-                gsap.to(image, { left: '0%', duration: 1.55, scale: 0.5, zIndex: 1 });
+                gsap.to(image, { left: '0%', duration: 1.55, scale: 0.6, zIndex: 1 });
             } 
             else if (leftPercentage > 59 & leftPercentage <= 61) {
-                gsap.to(image, { left: '30%', zIndex: 5, duration: 1.5, scale: 1 });
+                gsap.to(image, { left: '30%', zIndex: 5, duration: 1.5, scale: 1.1 });
             }
         });
     },
